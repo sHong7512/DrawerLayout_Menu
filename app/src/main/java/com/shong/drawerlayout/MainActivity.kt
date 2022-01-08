38b0c3fa9c58e7a17d9a64968e138c12b7826f55
SoonHong Kwon <ksh7512@uangel.com>
2022-01-08 18:09:57 +0900

    Navigate BackButton

--- app/src/main/java/com/shong/drawerlayout/MainActivity.kt
+++ app/src/main/java/com/shong/drawerlayout/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.snackbar.Snackbar
 import com.shong.drawerlayout.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.mainItem.leftMenuButton.setOnClickListener {
@@ -53,5 +58,28 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+    }
+
+    override fun onBackPressed() {
+        when {
+            binding.drawerLayout.isDrawerOpen(binding.leftMenuItem.leftMenuLayout) -> binding.drawerLayout.closeDrawer(binding.leftMenuItem.leftMenuLayout)
+            binding.drawerLayout.isDrawerOpen(binding.rightMenuItem.rightMenuLayout) -> binding.drawerLayout.closeDrawer(binding.rightMenuItem.rightMenuLayout)
+            else -> backDoubleCount()
+        }
+    }
+
+    private var backDoubleCounting = 0
+    private fun backDoubleCount(){
+        backDoubleCounting++
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            backDoubleCounting = 0
+        }
+
+        if(backDoubleCounting > 1){
+            super.onBackPressed()
+        }else{
+            Snackbar.make(binding.root,"[알림] '뒤로'버튼을 한번더 누르면 종료됩니다.", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
